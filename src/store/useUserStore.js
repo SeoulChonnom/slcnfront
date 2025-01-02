2e59cd80d21a4c882f35558b50c3b9d1cde1f7e3
SeoulChonnom <bk688035@gmail.com>
2025-01-02 23:30:13 +0900

    feat: useUserStore 추가

---
+++ src/store/useUserStore.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import config from "@/config"
+
+export const useUserStore = defineStore("user", {
+    persist: {
+        storage: sessionStorage,
+        paths: ["userInfo"]
+    },
+    state: () => ({
+        token: null,
+        userInfo: {},
+    }),
+    getters: {
+        isLogin(state) {
+            return !!state.token;
+        }
+    },
+    actions: {
+        login(userName, password) {
+            console.log(config.account.postLogin);
+            axios({
+                url: config.account.postLogin,
+                method: "POST",
+                data: {
+                        username: userName,
+                        password: password
+                }
+            }).then((res) => {
+                console.log(res.data);
+            })
+        }
+    }
+})
