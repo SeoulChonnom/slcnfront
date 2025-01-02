5d0b2033a30d7591f8ade0dbe0bdf1ed76326117
SeoulChonnom <bk688035@gmail.com>
2025-01-02 23:35:13 +0900

    feat: useUserStore 리프레쉬 토큰 메서드 추가

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -17,18 +17,45 @@ export const useUserStore = defineStore("user", {
     },
   },
   actions: {
-    login(userName, password) {
-      console.log(config.account.postLogin);
-      axios({
-        url: config.account.postLogin,
-        method: "POST",
-        data: {
-          username: userName,
-          password: password,
-        },
-      }).then((res) => {
-        console.log(res.data);
-      });
+    async loginByInput(userName, password) {
+      try {
+        const res = await axios({
+          url: config.account.postLogin(),
+          method: "POST",
+          data: {
+            username: userName,
+            password: password,
+          },
+        });
+        this.token = res.data.data.accessToken;
+        this.userInfo = {
+          name: res.data.data.name,
+          userName: res.data.data.username,
+          roleList: res.data.data.roleList,
+        };
+      } catch (e) {
+        if (e.response && e.status === 400) {
+          throw new Error(e.response.data.message);
+        } else {
+          throw new Error("알 수 없는 오류입니다.");
+        }
+      }
+    },
+    async loginByRefreshToken() {
+      try {
+        const res = await axios({
+          url: config.account.getToken(),
+          method: "GET",
+        });
+        this.token = res.data.data.accessToken;
+        this.userInfo = {
+          name: res.data.data.name,
+          userName: res.data.data.username,
+          roleList: res.data.data.roleList,
+        };
+      } catch (e) {
+        throw new Error(e.response.data.message);
+      }
     },
   },
 });
