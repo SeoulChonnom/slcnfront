6da97bafc7d65e0aa29efe430c7559422b72193f
SeoulChonnom <bk688035@gmail.com>
2025-01-24 00:32:20 +0900

    feat: tripList 로직 수정

--- src/components/trip/tripList.vue
+++ src/components/trip/tripList.vue
@@ -4,64 +4,86 @@
       <img class="map" :src="trip.logo" />
     </div>
     <div class="mapDesc">
-      {{ trip.desc1 }}<br />
-      {{ trip.desc2 }}
+      {{ trip.info1 }}<br />
+      {{ trip.info2 }}
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import swal from "sweetalert2";
+import { defineProps } from "vue";
+import { useRouter } from "vue-router";
 
-export default {
-  name: "mapList",
-  props: { trip: Object },
-  methods: {
-    async onClickMap() {
-      const inputOptions = new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(this.trip.radio);
-        }, 500);
-      });
-      const { value: ans } = await swal.fire({
-        icon: "question",
-        title: this.trip.title,
-        input: "radio",
-        inputOptions,
-        inputValidator: (value) => {
-          if (!value) {
-            return "정답을 선택해주세요!";
-          }
-        },
-      });
-      if (ans === this.trip.answer) {
-        swal
-          .fire({
-            icon: "success",
-            title: this.trip.answerTitle,
-            text: this.trip.answerText,
-            timer: 1000,
-            timerProgressBar: true,
-            showConfirmButton: false,
-          })
-          .then(() => {
-            this.$router.push("/map/" + this.trip.date);
-          });
-      } else {
-        swal.fire({
-          icon: "error",
-          title: this.trip.errorTitle,
-          text: this.trip.errorText,
-          timer: 1000,
-          timerProgressBar: true,
-          showConfirmButton: false,
-        });
+const router = useRouter();
+const props = defineProps({
+  trip: Object,
+});
+
+const makeQuizAnswer = () => {
+  const quiz = {
+    answer: undefined,
+    responses: [],
+  };
+
+  props.trip.quizResponses.forEach((response) => {
+    quiz.responses.push(response.answer);
+    if (response.quizIndex === props.trip.quizAnswer) {
+      quiz.answer = response.answer;
+    }
+  });
+
+  return quiz;
+};
+
+const isCorrect = (answer, idx) => {
+  const response = props.trip.quizResponses.find(
+    (response) => response.quizIndex === idx
+  );
+  return response ? answer === response.answer : false;
+};
+
+const onClickMap = async () => {
+  const quiz = makeQuizAnswer();
+  const inputOptions = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(quiz.responses);
+    }, 500);
+  });
+  const { value: ans } = await swal.fire({
+    icon: "question",
+    title: props.trip.quizTitle.replace("\\n", "\n"),
+    input: "radio",
+    inputOptions,
+    inputValidator: (value) => {
+      if (!value) {
+        return "정답을 선택해주세요!";
       }
     },
-  },
-  data() {
-    return {};
-  },
+  });
+  if (isCorrect(quiz.answer, ans)) {
+    swal
+      .fire({
+        icon: "success",
+        title: props.trip.quizAnswerTitle,
+        text: props.trip.quizAnswerText,
+        timer: 1000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+      })
+      .then(() => {
+        router.push("/map/" + props.trip.date);
+      });
+  } else {
+    swal.fire({
+      icon: "error",
+      title: props.trip.quizErrorTitle,
+      text: props.trip.quizErrorText,
+      timer: 1000,
+      timerProgressBar: true,
+      showConfirmButton: false,
+    });
+  }
 };
 </script>
 
