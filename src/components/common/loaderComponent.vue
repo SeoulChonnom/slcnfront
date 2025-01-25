b80776becb8ba5f91749a635ec4f738a09fb72f2
SeoulChonnom <bk688035@gmail.com>
2025-01-26 02:43:05 +0900

    feat: 페이지 로더 추가

---
+++ src/components/common/loaderComponent.vue
@@ -0,0 +1,298 @@
+<template>
+  <div id="loader-main">
+    <div class="loader-spinner">
+      <div class="loader-spinner-inner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+@keyframes loader-spinner-inner {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+#loader-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+.loader-spinner-inner div {
+  left: 80px;
+  top: 50px;
+  position: absolute;
+  animation: loader-spinner-inner linear 1s infinite;
+  background: #f1d6d0;
+  width: 40px;
+  height: 20px;
+  border-radius: 20px / 10px;
+  transform-origin: 20px 50px;
+}
+.loader-spinner-inner div:nth-child(1) {
+  transform: rotate(0deg);
+  animation-delay: -0.975s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(2) {
+  transform: rotate(9deg);
+  animation-delay: -0.95s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(3) {
+  transform: rotate(18deg);
+  animation-delay: -0.925s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(4) {
+  transform: rotate(27deg);
+  animation-delay: -0.9s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(5) {
+  transform: rotate(36deg);
+  animation-delay: -0.875s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(6) {
+  transform: rotate(45deg);
+  animation-delay: -0.85s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(7) {
+  transform: rotate(54deg);
+  animation-delay: -0.825s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(8) {
+  transform: rotate(63deg);
+  animation-delay: -0.8s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(9) {
+  transform: rotate(72deg);
+  animation-delay: -0.775s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(10) {
+  transform: rotate(81deg);
+  animation-delay: -0.75s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(11) {
+  transform: rotate(90deg);
+  animation-delay: -0.725s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(12) {
+  transform: rotate(99deg);
+  animation-delay: -0.7s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(13) {
+  transform: rotate(108deg);
+  animation-delay: -0.675s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(14) {
+  transform: rotate(117deg);
+  animation-delay: -0.65s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(15) {
+  transform: rotate(126deg);
+  animation-delay: -0.625s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(16) {
+  transform: rotate(135deg);
+  animation-delay: -0.6s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(17) {
+  transform: rotate(144deg);
+  animation-delay: -0.575s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(18) {
+  transform: rotate(153deg);
+  animation-delay: -0.55s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(19) {
+  transform: rotate(162deg);
+  animation-delay: -0.525s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(20) {
+  transform: rotate(171deg);
+  animation-delay: -0.5s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(21) {
+  transform: rotate(180deg);
+  animation-delay: -0.475s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(22) {
+  transform: rotate(189deg);
+  animation-delay: -0.45s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(23) {
+  transform: rotate(198deg);
+  animation-delay: -0.425s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(24) {
+  transform: rotate(207deg);
+  animation-delay: -0.4s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(25) {
+  transform: rotate(216deg);
+  animation-delay: -0.375s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(26) {
+  transform: rotate(225deg);
+  animation-delay: -0.35s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(27) {
+  transform: rotate(234deg);
+  animation-delay: -0.325s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(28) {
+  transform: rotate(243deg);
+  animation-delay: -0.3s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(29) {
+  transform: rotate(252deg);
+  animation-delay: -0.275s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(30) {
+  transform: rotate(261deg);
+  animation-delay: -0.25s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(31) {
+  transform: rotate(270deg);
+  animation-delay: -0.225s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(32) {
+  transform: rotate(279deg);
+  animation-delay: -0.2s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(33) {
+  transform: rotate(288deg);
+  animation-delay: -0.175s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(34) {
+  transform: rotate(297deg);
+  animation-delay: -0.15s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(35) {
+  transform: rotate(306deg);
+  animation-delay: -0.125s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(36) {
+  transform: rotate(315deg);
+  animation-delay: -0.1s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(37) {
+  transform: rotate(324deg);
+  animation-delay: -0.075s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(38) {
+  transform: rotate(333deg);
+  animation-delay: -0.05s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(39) {
+  transform: rotate(342deg);
+  animation-delay: -0.025s;
+  background: #f1d6d0;
+}
+.loader-spinner-inner div:nth-child(40) {
+  transform: rotate(351deg);
+  animation-delay: 0s;
+  background: #f1d6d0;
+}
+.loader-spinner {
+  width: 200px;
+  height: 200px;
+  display: inline-block;
+  overflow: hidden;
+  background: #ffffff;
+}
+.loader-spinner-inner {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform: translateZ(0) scale(1);
+  backface-visibility: hidden;
+  transform-origin: 0 0; /* see note above */
+}
+.loader-spinner-inner div {
+  box-sizing: content-box;
+}
+</style>
