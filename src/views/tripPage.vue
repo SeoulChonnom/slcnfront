61edf4afc7ab77269c749c416e6bedc86ef8b8b8
SeoulChonnom <bk688035@gmail.com>
2025-01-26 02:43:25 +0900

    feat: 나들이 페이지 로직 변경

--- src/views/tripPage.vue
+++ src/views/tripPage.vue
@@ -2,10 +2,10 @@
   <div id="content">
     <div id="infoDiv">서울 촌놈 나들이 경로 😎</div>
     <div id="mapDiv">
-      <img v-if="showMap" class="map" id="map1" :src="getMap1()" />
-      <img v-if="!showMap" class="map" id="map2" :src="getMap2()" />
+      <img v-if="isMap1" class="map" id="map1" :src="map1" />
+      <img v-if="!isMap1" class="map" id="map2" :src="map2" />
     </div>
-    <div v-if="getMap2()" id="move">
+    <div v-if="isMultiMapTrip()" id="move">
       <button id="moveButton" @click="moveButtonClick()">
         {{ getMoveButtonText() }}
       </button>
@@ -22,51 +22,82 @@
   </div>
 </template>
 
-<script>
-import { globalTrip } from "@/global/global";
+<script setup>
+import { ref, onBeforeMount, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { useTripStore } from "@/store/useTripStore";
 
-export default {
-  name: "tripPage",
-  data() {
-    return {
-      showMap: true,
-      globalTrip,
-      date: this.$route.params.date,
-    };
-  },
-  methods: {
-    getMap1() {
-      return globalTrip.trips.find((item) => item.date === this.date).map;
-    },
-    getDrive() {
-      return globalTrip.trips.find((item) => item.date === this.date).drive;
-    },
-    getMap2() {
-      return globalTrip.trips.find((item) => item.date === this.date).map2;
-    },
-    getButtonText1() {
-      return globalTrip.trips.find((item) => item.date === this.date)
-        .buttonText1;
-    },
-    getButtonText2() {
-      return globalTrip.trips.find((item) => item.date === this.date)
-        .buttonText2;
-    },
-    getMoveButtonText() {
-      if (this.showMap) {
-        return this.getButtonText1();
-      }
-      return this.getButtonText2();
-    },
-    moveButtonClick() {
-      this.showMap = !this.showMap;
-    },
-  },
-  onMounted() {
-    console.log(globalTrip.getTrip(this.date));
-    console.log(this.date);
-  },
+const isMap1 = ref(true);
+const route = useRoute();
+const tripStore = useTripStore();
+const date = route.params.date;
+const tripData = ref({
+  date: null,
+  map1: null,
+  map2: null,
+  button1: null,
+  button2: null,
+  drive: null,
+});
+const map1 = ref(null);
+const map2 = ref(null);
+
+const moveButtonClick = () => {
+  isMap1.value = !isMap1.value;
 };
+
+const isMultiMapTrip = () => {
+  if (tripData.value.map2?.length > 0) {
+    return true;
+  }
+  return false;
+};
+
+const getMap1 = () => {
+  tripStore.getFile(tripData.value.map1).then((data) => {
+    map1.value = data;
+  });
+};
+
+const getMap2 = () => {
+  if (isMultiMapTrip()) {
+    tripStore.getFile(tripData.value.map2).then((data) => {
+      map2.value = data;
+    });
+  } else {
+    map2.value = map1.value;
+  }
+};
+
+const getDrive = () => {
+  return tripData.value.drive;
+};
+
+const getButtonText1 = () => {
+  return tripData.value.button1;
+};
+
+const getButtonText2 = () => {
+  return tripData.value.button2;
+};
+
+const getMoveButtonText = () => {
+  if (isMap1.value) {
+    return getButtonText1();
+  }
+  return getButtonText2();
+};
+
+onBeforeMount(() => {});
+
+onMounted(() => {
+  tripStore.findTripInfo(date).then((data) => {
+    tripData.value = data;
+    getMap1();
+    getMap2();
+    console.log(tripData.value);
+  });
+});
 </script>
 
 <style scoped>
