56eb5a893cb5ce7c5e13ab07ce3cdc3cf4868b06
SeoulChonnom <bk688035@gmail.com>
2025-04-25 17:40:22 +0900

    feat: 서비스 2개 추가

--- src/service/scheduleService.js
+++ src/service/scheduleService.js
@@ -2,11 +2,46 @@ import axios from 'axios';
 import { useUserStore } from '@/store/useUserStore';
 import config from '@/config';
 
-export const getCalendarList = async () => {
+export const getSchedulesForNow = async () => {
   try {
     const res = await axios({
       url: config.schedule.getScheduleList(),
       method: 'GET',
+      headers: { 'X-AUTH-TOKEN': useUserStore().token },
+    });
+    return [...res.data.data];
+  } catch (e) {
+    if (e.response?.status === 400) {
+      throw new Error(e.response.data.message);
+    } else {
+      throw new Error('알 수 없는 오류입니다.');
+    }
+  }
+};
+
+export const getSchedulesForYearAndMonth = async (year, month) => {
+  try {
+    const res = await axios({
+      url: config.schedule.getScheduleListForYearAndMonth(year, month),
+      method: 'GET',
+      headers: { 'X-AUTH-TOKEN': useUserStore().token },
+    });
+    return [...res.data.data];
+  } catch (e) {
+    if (e.response?.status === 400) {
+      throw new Error(e.response.data.message);
+    } else {
+      throw new Error('알 수 없는 오류입니다.');
+    }
+  }
+};
+
+export const registerCalendar = async (event) => {
+  try {
+    const res = await axios({
+      url: config.schedule.registerSchedule(),
+      method: 'POST',
+      data: event,
       headers: { 'X-AUTH-TOKEN': useUserStore().token },
     });
     return [...res.data.data];
